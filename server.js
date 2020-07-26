644a3151e10e93187ed965f7c6b393a339c37712
raymond <raymondomon@yahoo.com>
2020-07-26 10:02:17 +0100

    minor changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
-
 const port = 3000;
 let queryData = "";
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
